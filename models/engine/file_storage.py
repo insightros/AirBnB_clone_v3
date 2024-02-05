0efecd1bc0fa75a4b469da0a2971178b37ded95f
xeno <xeno@xeno.0.2.15>
2024-02-05 13:51:08 +0100

    airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the FileStorage class
-"""
+"Contains File Storage"
 
 import json
 import models
@@ -19,7 +17,6 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
 
     # string - path to the JSON file
     __file_path = "file.json"
@@ -37,13 +34,11 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
             if key == "password":
@@ -53,7 +48,6 @@ class FileStorage:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
@@ -63,21 +57,15 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
     def get(self, cls, id):
-        """
-        Returns the object based on the class name and its ID, or
-        None if not found
-        """
         if cls not in classes.values():
             return None
 
@@ -89,9 +77,6 @@ class FileStorage:
         return None
 
     def count(self, cls=None):
-        """
-        count the number of objects in storage
-        """
         all_class = classes.values()
 
         if not cls:
